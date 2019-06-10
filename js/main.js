00df31d4a7804bd833630fd8dccf2cb4b6092d58
pareen89 <pareenpatel89@gmail.com>
2019-06-10 16:27:59 -0700

    added time and date to taskbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
-console.log('testing');
+let date = document.getElementById('date');
+let clock = document.getElementById('clock');
+function timeAndDate() {
+  let now = new Date();
+  let hour = now.getHours();
+  let min = now.getMinutes();
+  let month = now.getMonth() + 1;
+  let day = now.getDate();
+  let year = now.getFullYear();
+  let time = '';
+  // time and date format(time= hh:mm and date = mm/dd/yyyy )
+  if (min < 10) {
+    min = `0${min}`;
+  }
+  if (hour > 12) {
+    time = `${hour - 12}:${min} PM`;
+  } else if (hour === 0) {
+    hour = 12;
+    time = `${hour}:${min} AM`;
+  } else {
+    time = `${hour}:${min} AM`;
+  }
+
+  let date = `${month}/${day}/${year}`;
+  // assigning time and date to website
+  clock.innerHTML = time;
+  document.getElementById('date').innerHTML = date;
+}
+let update = setInterval(timeAndDate, 1000);
+//
