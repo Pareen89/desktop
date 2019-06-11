a9b0a18b9592322133a2edd1d0948545e901ec51
pareen89 <pareenpatel89@gmail.com>
2019-06-11 11:46:30 -0700

    Added timeDate section

--- js/main.js
+++ js/main.js
@@ -1,29 +1,39 @@
 let date = document.getElementById('date');
 let clock = document.getElementById('clock');
+let clockTwo = document.getElementById('clockTwo');
 let currentTemp = document.getElementById('temp');
-window.addEventListener('load', () => {
-  let long;
-  let lat;
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(position => {
-      long = position.coords.longitude;
-      lat = position.coords.latitude;
-      const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=4da5d012ad5456829aaa191102f8a340`;
-      console.log(api);
-      fetch(api)
-        .then(response => {
-          return response.json();
-        })
-        .then(data => {
-          const temp = data.main.temp;
-          let kelvin = 273.15;
-          let celsius = temp - kelvin;
-          let fahrenheit = Math.round((temp - kelvin) * (9 / 5) + 32);
-          currentTemp.innerHTML = fahrenheit;
-        });
-    });
-  }
+let box1 = document.getElementById('box1');
+let timeDateShow = document.getElementById('timeDateShow');
+let clockSet = document.getElementById('check');
+
+box1.addEventListener('click', function() {
+  console.log('clicked');
+  timeDateShow.classList.toggle('timeDateShow');
 });
+
+// Temp
+// window.addEventListener('load', () => {
+//   let long;
+//   let lat;
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(position => {
+//       long = position.coords.longitude;
+//       lat = position.coords.latitude;
+//       const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=4da5d012ad5456829aaa191102f8a340`;
+//       fetch(api)
+//         .then(response => {
+//           return response.json();
+//         })
+//         .then(data => {
+//           const temp = data.main.temp;
+//           let kelvin = 273.15;
+//           let celsius = temp - kelvin;
+//           let fahrenheit = Math.round((temp - kelvin) * (9 / 5) + 32);
+//           currentTemp.innerHTML = fahrenheit;
+//         });
+//     });
+//   }
+// });
 
 // Time and Date
 function timeAndDate() {
@@ -32,25 +42,40 @@ function timeAndDate() {
   let min = now.getMinutes();
   let month = now.getMonth() + 1;
   let day = now.getDate();
+  let days = now.getDay();
   let year = now.getFullYear();
+  let seconds = now.getSeconds();
   let time = '';
+  let timeTwo = '';
+  let set = '';
   // time and date format(time= hh:mm and date = mm/dd/yyyy )
   if (min < 10) {
     min = `0${min}`;
   }
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
   if (hour > 12) {
+    set = 'PM';
     time = `${hour - 12}:${min} PM`;
+    timeTwo = `${hour - 12}:${min}:${seconds}`;
   } else if (hour === 0) {
+    set = 'AM';
     hour = 12;
     time = `${hour}:${min} AM`;
+    timeTwo = `${hour}:${min}:${seconds}`;
   } else {
+    set = 'AM';
     time = `${hour}:${min} AM`;
+    timeTwo = `${hour}:${min}:${seconds}`;
   }
 
-  let date = `${month}/${day}/${year}`;
+  let d = `${month}/${day}/${year}`;
   // assigning time and date to website
   clock.innerHTML = time;
-  document.getElementById('date').innerHTML = date;
+  clockTwo.innerHTML = timeTwo;
+  date.innerHTML = d;
+  clockSet.innerHTML = set;
 }
 let update = setInterval(timeAndDate, 1000);
 let temp = '';
