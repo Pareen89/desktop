90212957edf6eb5ccf0c014ae8050bd4d4b572cd
pareen89 <pareenpatel89@gmail.com>
2019-06-19 13:00:22 -0700

    updated temp in js file

--- js/main.js
+++ js/main.js
@@ -13,29 +13,29 @@ box1.addEventListener('click', function() {
   timeDateShow.classList.toggle('timeDateShow');
 });
 
-// Temp
-// window.addEventListener('load', () => {
-//   let long;
-//   let lat;
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(position => {
-//       long = position.coords.longitude;
-//       lat = position.coords.latitude;
-//       const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=4da5d012ad5456829aaa191102f8a340`;
-//       fetch(api)
-//         .then(response => {
-//           return response.json();
-//         })
-//         .then(data => {
-//           const temp = data.main.temp;
-//           let kelvin = 273.15;
-//           let celsius = temp - kelvin;
-//           let fahrenheit = Math.round((temp - kelvin) * (9 / 5) + 32);
-//           currentTemp.innerHTML = fahrenheit;
-//         });
-//     });
-//   }
-// });
+Temp;
+window.addEventListener('load', () => {
+  let long;
+  let lat;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=4da5d012ad5456829aaa191102f8a340`;
+      fetch(api)
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          const temp = data.main.temp;
+          let kelvin = 273.15;
+          let celsius = temp - kelvin;
+          let fahrenheit = Math.round((temp - kelvin) * (9 / 5) + 32);
+          currentTemp.innerHTML = fahrenheit;
+        });
+    });
+  }
+});
 
 // Time and Date
 function timeAndDate() {
